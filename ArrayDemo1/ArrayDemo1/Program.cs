f21bacc8eaa9532e04e27a6e214a7b8bf3050eb5
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-08 10:18:45 -0600

    finished

--- ArrayDemo1/ArrayDemo1/Program.cs
+++ ArrayDemo1/ArrayDemo1/Program.cs
@@ -6,7 +6,11 @@ namespace ArrayDemo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] payRate = { 6.00, 7.35, 8.12, 12.45 };
+            for (int x = 0; x < payRate.Length; ++x)
+            {
+                Console.WriteLine("Pay rate {0} is {1}", x, payRate[x].ToString("C"));
+            }
         }
     }
 }
