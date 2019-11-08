7d586e059a1476e8da6da2c74be7a5d4881602b1
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-08 10:28:22 -0600

    finished

--- ArrayDemo2/ArrayDemo2/Program.cs
+++ ArrayDemo2/ArrayDemo2/Program.cs
@@ -6,7 +6,35 @@ namespace ArrayDemo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] scores = new int[8];
+            int x;
+            string inputString;
+            for (x = 0; x < scores.Length; ++x)
+            {
+                Console.Write("Enter your score on test {0} ", x + 1);
+                inputString = Console.ReadLine();
+                scores[x] = Convert.ToInt32(inputString);
+            }
+            Console.WriteLine("\n--------------------");
+            Console.WriteLine("Scores in original order:");
+            for (x = 0; x < scores.Length; ++x)
+            {
+                Console.Write("{0, 6}", scores[x]);
+            }
+            Console.WriteLine("\n--------------------");
+            Array.Sort(scores);
+            Console.WriteLine("Scores in sorted order:");
+            for (x = 0; x < scores.Length; ++x)
+            {
+                Console.Write("{0, 6}", scores[x]);
+            }
+            Console.WriteLine("\n--------------------");
+            Array.Reverse(scores);
+            Console.WriteLine("Scores in reverse order:");
+            for (x = 0; x < scores.Length; ++x)
+            {
+                Console.Write("{0, 6}", scores[x]);
+            }
         }
     }
 }
